b579f7e7b1679d8887bb275783b4a849db8fd0bb
Katerina Strucheva <ekaterina.strucheva@travelline.ru>
2024-06-06 17:55:16 +0300

    added readme, reports example, a choice of format

--- adepter/src/main/java/Main.java
+++ adepter/src/main/java/Main.java
@@ -5,11 +5,39 @@ import reportGenerator.typeReport.ExcelReporter;
 import reportGenerator.typeReport.PDFReporter;
 
 import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ReportGenerator reportGenerator = new ExcelReportAdapter();
+
+        ReportGenerator reportGenerator =  selectFormat();
         reportGenerator.generateReport("Report for: " + new Date().toString());
+    }
+
+    public static ReportGenerator selectFormat() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Выбираем формат: PDF, XLS");
+        String format = in.nextLine().toLowerCase();
+        in.close();
+
+        return setGenerator(format);
+    }
+
+    public static ReportGenerator setGenerator(String format) {
+        ReportGenerator reportGenerator;
+        switch (format) {
+            case "pdf":
+                System.out.println("==== Your choice: PDF ====");
+                reportGenerator = new PDFReportAdapter();
+                return reportGenerator;
+            case "xls":
+                System.out.println("==== Your choice: XLS ====");
+                reportGenerator = new ExcelReportAdapter();
+                return reportGenerator;
+            default:
+                System.out.println("неизвестный формат");
+                return null;
+        }
     }
 }
 
