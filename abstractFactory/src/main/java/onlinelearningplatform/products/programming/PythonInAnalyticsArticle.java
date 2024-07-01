1fb66b9b980960bb6cc093accb7d4bdeab3a04a0
Katerina Strucheva <ekaterina.strucheva@travelline.ru>
2024-07-01 11:43:15 +0300

    Added article for programming in products

---
+++ abstractFactory/src/main/java/onlinelearningplatform/products/programming/PythonInAnalyticsArticle.java
@@ -0,0 +1,27 @@
+package onlinelearningplatform.products.programming;
+import onlinelearningplatform.products.typeofcontent.Article;
+
+public class PythonInAnalyticsArticle implements Article {
+    String title;
+
+    public PythonInAnalyticsArticle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public void publishArticle() {
+        System.out.println("Успех!"
+                + "\n Вы опубликовали статью про анализ данных с ипользлванием пайтон ");
+    }
+
+    @Override
+    public void openArticle() {
+        System.out.println("Тут открылась статья '" + title + "'");
+    }
+
+    @Override
+    public void showBriefReport() {
+        System.out.println("В кратце - сконвертирую мого циферок в красивые визулизации");
+    }
+
+}
